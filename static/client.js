d982288e6caa93f836a37aae8bf0729bb5d0f17b
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-02-13 15:59:43 +0100

    Place a call via IQ rpc

--- static/client.js
+++ static/client.js
@@ -1,9 +1,25 @@
+var pc = new mozRTCPeerConnection();
+pc.onaddstream = function(obj) {
+  var type = obj.type;
+  if (type == "video") {
+    var video = $("#remoteVideo")[0];
+    video.mozSrcObject = obj.stream;
+    video.play();
+  } else if (type == "audio") {
+    var audio = $("#remoteAudio")[0];
+    audio.mozSrcObject = obj.stream;
+    audio.play();
+  } else {
+    alert("sender onaddstream of unknown type, obj = " + obj.toSource());
+  }
+};
+
 function log(msg) {
   $('#log').append('<div></div>').append(document.createTextNode(msg));
 }
 
 $(document).ready(function () {
-  var xmpp = new Lightstring.Connection('ws://localhost:5280');
+  var xmpp = new Lightstring.Connection('ws://' + document.location.hostname + ':5280');
   xmpp.load('DIGEST-MD5', 'presence', 'im', 'roster');
 
   xmpp.on('connecting', function(stanza) {
@@ -22,11 +38,11 @@ $(document).ready(function () {
 
   xmpp.on('presence', function(stanza) {
     if (stanza.attrs.type)
-        return;
+      return;
 
     var jid = new Lightstring.JID(stanza.attrs.from).toBare().toString();
     var link = $('<a href="#">' + jid + '</a>').click(function() {
-        call(xmpp, stanza.attrs.from);
+      call(xmpp, stanza.attrs.from);
     });
     $('#contacts ul li[data-jid="' + jid.toString() + '"]').html(link);
   });
@@ -34,13 +50,27 @@ $(document).ready(function () {
   xmpp.on('iq', function(stanza) {
     if (!stanza.getChild('offer'))
       return;
-    var offer = stanza.getChild('offer').text();
-    var iq = '<iq from="' + xmpp.jid + '" to="' + stanza.attrs.from +'" type="result">' +
-               '<answer xmlns="webrtc:iq:sdp">' +
-                 offer +
-               '</answer>' +
-             '</iq>'
-    xmpp.send(iq);
+
+    var offer = JSON.parse(stanza.getChild('offer').text());
+    navigator.mozGetUserMedia({video: true, audio: true}, function(stream) {
+      var video = $("#localVideo")[0];
+      video.mozSrcObject = stream;
+      video.play();
+      pc.addStream(stream);
+      pc.setRemoteDescription(offer, function() {
+        pc.createAnswer(function(answer) {
+          pc.setLocalDescription(answer, function() {
+            answer = JSON.stringify(answer);
+            var iq = '<iq from="' + xmpp.jid + '" to="' + stanza.attrs.from +'" type="result" id="' + stanza.attrs.id + '">' +
+                       '<answer xmlns="webrtc:iq:sdp">' +
+                         answer +
+                       '</answer>' +
+                     '</iq>'
+            xmpp.send(iq);
+          }, function(err) { alert("setLocalDescription failed: " + err) });
+        }, function(err) { alert("createAnswer failed: " + err); });
+      }, function(err) { alert("setRemoteDescription failed: " + err); });
+    }, function(err) { alert("getUserMedia failed: "  + err); });
   });
 
   xmpp.on('connected', function(stanza) {
@@ -74,15 +104,26 @@ $(document).ready(function () {
 });
 
 function call(xmpp, to) {
-  var offer = 'nothing relevant for now';
-  var iq = '<iq from="' + xmpp.jid + '" to="' + to +'" type="set">' +
-             '<offer xmlns="webrtc:iq:sdp">' +
-               offer +
-             '</offer>' +
-           '</iq>'
-  xmpp.send(iq, function(stanza) {
-    var answer = stanza.getChild().text();
-    console.log(anwser);
-  });
+  navigator.mozGetUserMedia({video: true, audio: true}, function(stream) {
+    var video = $("#localVideo")[0];
+    video.mozSrcObject = stream;
+    video.play();
+    pc.addStream(stream);
+    pc.createOffer(function(offer) {
+      pc.setLocalDescription(offer, function() {
+        var iq = '<iq from="' + xmpp.jid + '" to="' + to +'" type="set">' +
+                   '<offer xmlns="webrtc:iq:sdp">' +
+                     JSON.stringify(offer) +
+                   '</offer>' +
+                 '</iq>';
+
+        xmpp.send(iq, function(stanza) {
+          var answer = JSON.parse(stanza.getChild('answer').text());
+          pc.setRemoteDescription(answer, function() {});
+        });
+
+      }, function(err) { alert("setLocalDescription failed: " + err) });
+    }, function(err) { alert("createOffer failed: " + err); });
+  }, function(err) { alert("getUserMedia failed: "  + err); });
 }
 
